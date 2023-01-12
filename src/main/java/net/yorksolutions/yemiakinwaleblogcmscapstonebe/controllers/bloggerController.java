7869d73e69b2679859875d4ba87f685f74267332
York MacBook 031 <yorkmacbook031@York-MacBook-031.local>
2023-01-12 08:52:07 -0600

    blogger

--- src/main/java/net/yorksolutions/yemiakinwaleblogcmscapstonebe/controllers/bloggerController.java
+++ src/main/java/net/yorksolutions/yemiakinwaleblogcmscapstonebe/controllers/bloggerController.java
@@ -1,4 +1,25 @@
 package net.yorksolutions.yemiakinwaleblogcmscapstonebe.controllers;
 
+import net.yorksolutions.yemiakinwaleblogcmscapstonebe.models.Blogger;
+import net.yorksolutions.yemiakinwaleblogcmscapstonebe.services.BloggerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@CrossOrigin
 public class BloggerController {
+
+    private final BloggerService service;
+
+    @Autowired
+    public BloggerController(BloggerService service) { this.service = service;}
+
+//    @GetMapping
+//    public Iterable <Blogger> getAllBlogger() {
+//        return service.getAllBlogger();
 }
+
--- src/main/java/net/yorksolutions/yemiakinwaleblogcmscapstonebe/services/bloggerService.java
+++ src/main/java/net/yorksolutions/yemiakinwaleblogcmscapstonebe/services/bloggerService.java
@@ -1,4 +1,20 @@
 package net.yorksolutions.yemiakinwaleblogcmscapstonebe.services;
 
+import net.yorksolutions.yemiakinwaleblogcmscapstonebe.repositories.BlogRepository;
+import net.yorksolutions.yemiakinwaleblogcmscapstonebe.repositories.BloggerRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BloggerService {
+
+    private final BloggerRepository bloggerRepository;
+
+    private final BlogRepository blogRepository;
+
+    public BloggerService(BloggerRepository bloggerRepository, BlogRepository blogRepository) {
+        this.bloggerRepository = bloggerRepository;
+        this.blogRepository = blogRepository;
+    }
+
+
 }
